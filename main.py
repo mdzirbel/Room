29b2fcdd78e7de12136630a8ef6b31508e0b22ce
Maxwell <maxfojtik@bex.net>
2019-02-23 02:57:08 -0500

    make it not init lights in gui mode

--- main.py
+++ main.py
@@ -46,8 +46,9 @@ def mainLoop():
             time.sleep(1)
 
             updateBrightness()
-            lightController.strip.setBrightness(int(brightness*2.55))
-            lightController.strip2.setBrightness(int(brightness * 2.55))
+            if mode == "ceil":
+                lightController.strip.setBrightness(int(brightness*2.55))
+                lightController.strip2.setBrightness(int(brightness * 2.55))
 
     finally:
         stopAllThreads()
