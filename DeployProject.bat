e376ad1df31b9c6cade8d33078c6962d59da47e1
Maxwell <maxfojtik@bex.net>
2019-03-27 22:39:08 -0400

    updated ip

--- DeployProject.bat
+++ DeployProject.bat
@@ -1,4 +1,4 @@
 py deployProject.py
-"C:/Program Files/Putty/plink" pi@128.146.101.84 -batch -pw 0517fic sudo killall python
-"C:/Program Files/Putty/pscp" -r -pw 0517fic Deploy\ pi@128.146.101.84:PythonProjects/Room/
+"C:/Program Files/Putty/plink" pi@128.146.101.27 -batch -pw 0517fic sudo killall python
+"C:/Program Files/Putty/pscp" -r -pw 0517fic Deploy\ pi@128.146.101.27:PythonProjects/Room/
 rem "C:/Program Files/Putty/plink" pi@128.146.101.84 -batch -pw 0517fic sudo python "PythonProjects/main.py"
